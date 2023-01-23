e977ac242ce7aab17e1e050e0240a5c2f50a5803
BiagioButta <biagiob940@gmail.com>
2023-01-23 12:02:37 +0100

    migration

--- database/migrations/2023_01_23_101925_create_colors_table.php
+++ database/migrations/2023_01_23_101925_create_colors_table.php
@@ -15,6 +15,9 @@ return new class extends Migration
     {
         Schema::create('colors', function (Blueprint $table) {
             $table->id();
+            $table->string('name', 100)->unique();
+            $table->string('slug', 255);
+            $table->string('hex_value', 10)->unique();
             $table->timestamps();
         });
     }
