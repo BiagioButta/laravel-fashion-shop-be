d8850dc14c3fdac72b879d35e4df4b6f8d660905
SalvatoreVillano <villano.salvatore9@gmail.com>
2023-01-26 16:41:57 +0100

    fixed api

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,6 @@
 <?php
 
-use App\Http\Controllers\Admin\ProductController;
+use App\Http\Controllers\Api\ProductController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\LeadController;
